dba0cc9eeaeb4e244c484a89ec00b3c9bb6ea39c
helloWerld <108149462+helloWerld@users.noreply.github.com>
2024-07-17 18:13:51 -0400

    duplicate and shuffle kittens, card logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -7,20 +7,58 @@ import { getRandomKittens } from '@/services';
 const GameBoard = () => {
 	const [numOfKittens, setNumOfKittens] = useState(6);
 	const [kittens, setKittens] = useState([]);
-	const [cardOne, setCardOne] = useState();
-	const [cardTwo, setCardTwo] = useState();
+	const [cardOne, setCardOne] = useState(null);
+	const [cardTwo, setCardTwo] = useState(null);
 	const [solvedKittensArray, setSolvedKittensArray] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [gameWin, setGameWin] = useState(false);
 
 	// Function to fetch kittens
 	const fetchKittens = async () => {
+		setLoading(true);
 		const response = await getRandomKittens(numOfKittens);
 		if (response.isError) {
 			alert(response.error);
 		} else {
 			console.log('API Reponse:', response.images);
-			setKittens(response.images);
+			//RANDOMIZE AND DUPLICATE KITTENS HERE
+			const duplicatedAndShuffledKittens = randomizeAndDuplicateKittens(
+				response.images
+			);
+			setKittens(duplicatedAndShuffledKittens);
 		}
+		setLoading(false);
 	};
+
+	// Function to duplicate and randomize kittens
+	const randomizeAndDuplicateKittens = (kittensArray) => {
+		console.log('Kittens Array: ', kittensArray);
+		let newArray = [];
+		kittensArray.forEach((kitten, index) => {
+			let kittenCopy1 = { ...kitten, id: index + 'copy1' };
+			let kittenCopy2 = { ...kitten, id: index + 'copy2' };
+
+			newArray.push(kittenCopy1, kittenCopy2);
+		});
+		console.log('Duplicated Kittens', newArray);
+		newArray = newArray.sort(() => Math.random() - 0.5);
+		console.log('Duplicated and Shuffled', newArray);
+		return newArray;
+	};
+
+	// Function to check matches
+	useEffect(() => {
+		if (cardTwo != null) {
+			const timeout = setTimeout(() => {
+				if (cardOne.url === cardTwo.url && cardOne.id != cardTwo.id) {
+					setSolvedKittensArray((prev) => [...prev, cardOne.url]);
+				}
+				setCardOne(null);
+				setCardTwo(null);
+			}, 500);
+			return () => clearTimeout(timeout);
+		}
+	}, [cardTwo]);
 
 	useEffect(() => {
 		fetchKittens();
@@ -28,18 +66,24 @@ const GameBoard = () => {
 
 	return (
 		<div className="flex flex-wrap flex-row gap-4 w-full justify-center">
-			{kittens.map((kitten) => (
-				<KittenCard
-					key={kitten?.id}
-					kitten={kitten}
-					cardOne={cardOne}
-					setCardOne={setCardOne}
-					cardTwo={cardTwo}
-					setCardTwo={setCardTwo}
-					solvedKittensArray={solvedKittensArray}
-					setSolvedKittensArray={setSolvedKittensArray}
-				/>
-			))}
+			{loading ? (
+				<p className="text-xl flex items-center justify-center h-64">
+					loading<span className="loading loading-dots loading-lg"></span>
+				</p>
+			) : (
+				kittens.map((kitten) => (
+					<KittenCard
+						key={kitten?.id}
+						kitten={kitten}
+						cardOne={cardOne}
+						setCardOne={setCardOne}
+						cardTwo={cardTwo}
+						setCardTwo={setCardTwo}
+						solvedKittensArray={solvedKittensArray}
+						setSolvedKittensArray={setSolvedKittensArray}
+					/>
+				))
+			)}
 		</div>
 	);
 };
--- src/components/KittenCard.jsx
+++ src/components/KittenCard.jsx
@@ -9,11 +9,45 @@ const KittenCard = ({
 	solvedKittensArray,
 	setSolvedKittensArray,
 }) => {
+	const handleClick = () => {
+		if (cardOne === null) {
+			setCardOne(kitten);
+			console.log('card one', cardOne);
+			return;
+		}
+		if (cardTwo === null) {
+			setCardTwo(kitten);
+			console.log('card two', cardTwo);
+			return;
+		}
+	};
+
 	return (
-		<div className="flex h-80 w-80 items-center justify-center bg-secondary rounded-lg overflow-clip">
-			{/* Click Me */}
-			<img src={kitten.url} className="object-cover w-full h-full" />
-		</div>
+		<button
+			onClick={handleClick}
+			disabled={cardOne && cardTwo}
+			className="flex h-80 w-80 items-center justify-center bg-secondary rounded-lg overflow-clip"
+		>
+			<p
+				className={`${
+					(cardOne == kitten ||
+						cardTwo === kitten ||
+						solvedKittensArray.includes(kitten.url)) &&
+					'hidden'
+				}`}
+			>
+				Click Me
+			</p>
+			<img
+				className={`${
+					cardOne != kitten &&
+					cardTwo != kitten &&
+					!solvedKittensArray.includes(kitten.url) &&
+					'hidden'
+				} object-cover w-full h-full`}
+				src={kitten.url}
+			/>
+		</button>
 	);
 };
 
